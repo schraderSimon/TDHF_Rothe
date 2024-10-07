fixed numerical problems in optimization

grid_solve_restricted/grid_HF.py
@@ -128,7 +128,7 @@ class solver():
             if err<1e-11:
                 break
         return self.C
-    def propagate(self,field,dt,Tmax):
+    def propagate(self,field,dt,Tmax,E0):
         C=self.C
         C_trajectory=[]
         t=0
@@ -148,7 +148,7 @@ class solver():
             #plt.plot(self.grid,abs(C[:,0])**2);plt.plot(self.grid,abs(C[:,1])**2);plt.show()
             C_trajectory.append(C)
             dipole_trajectory.append(self.get_dipole_moment(C))
-        filename="grid_solution"
+        filename="grid_solution_%.3f.npz"%E0
         np.savez(filename,gridpoints=self.grid,Cvals=C_trajectory,dipole=dipole_trajectory)
         return self.t,self.dipole,self.energy
     def propagate_onestep_second_order(self,C,field,t,dt):
@@ -205,8 +205,8 @@ if __name__ == "__main__":
     R_list=[-1.15, 1.15]
     Z_list=[3,1]
     alpha=0.5
-    gridsize = 15
-    num_gridpoints = 100
+    gridsize = 30
+    num_gridpoints = 800
     
     S=solver(gridsize,num_gridpoints,Z_list,R_list,alpha,nelec=sum(Z_list))
     orbs=S.C_init[:,:S.norb]
@@ -223,7 +223,7 @@ if __name__ == "__main__":
 
     plt.legend()
     plt.show()
-    E0 = 0.02  # Maximum field strength
+    E0 = float(sys.argv[1])  # Maximum field strength
     omega = 0.06075  # Laser frequency
     t_c = 2 * np.pi / omega  # Optical cycle
     n_cycles = 3
@@ -235,7 +235,7 @@ if __name__ == "__main__":
     plt.plot(t, fieldfunc(t))
 
     plt.show()
-    t, dipole, energy = S.propagate(fieldfunc, 0.05, tfinal)
+    t, dipole, energy = S.propagate(fieldfunc, 0.05, tfinal,E0)
     plt.plot(t, np.array(dipole))
     plt.ylim(-2.4, -0.6)
     plt.show()

grid_solve_restricted/plot_grid_solution.py
@@ -3,34 +3,41 @@ from numpy import pi,sqrt
 import matplotlib.pyplot as plt
 import os
 import sys
-num_gauss=int(sys.argv[1])
-name_gauss="linear_basis_orbitals_unitary_%d.npz"%num_gauss
-name_grid="grid_solution.npz"
-name_Rothe="Rothe_linearbasis.npz"
+fieldstrengh=float(sys.argv[2])
+num_gauss=20
+name_gauss="linearbasis_%.3f_%d.npz"%(fieldstrengh,num_gauss)
+name_grid="grid_solution_%.3f.npz"%fieldstrengh
 orbitals_grid=np.load(name_grid)
-rothe=np.load(name_Rothe)
 grid_grid=orbitals_grid['gridpoints']
 Cvals_grid=orbitals_grid['Cvals']
 dipmom_grid=orbitals_grid['dipole']
 orbitals_gauss=np.load(name_gauss)
-grid_gauss=orbitals_gauss['gridpoints']
-Cvals_gauss=orbitals_gauss['Cvals']
-dipmom_gauss=orbitals_gauss['dipole']
+dipmom_gauss=orbitals_gauss['x']
+times_gauss=orbitals_gauss['t']
 
-times=times_gauss=np.linspace(0,310,Cvals_gauss.shape[0])
 times_grid=np.linspace(0,310,Cvals_grid.shape[0])
-times_Rothe=rothe["t"]
-dipmom_Rothe=rothe["x"]
+
 dg=grid_grid[1]-grid_grid[0]
 plt.plot(times_grid,dipmom_grid,label="Grid")
 plt.plot(times_gauss,dipmom_gauss,label="Gauss")
+try:
+    name_Rothe="Rothe_wavefunctions%.3f_%d.npz"%(fieldstrengh,int(sys.argv[1]))
 
-plt.plot(times_Rothe,dipmom_Rothe,label="Rothe_linear")
+    rothe=np.load(name_Rothe)
+
+    times_Rothe=rothe["times"]
+    dipmom_Rothe=rothe["xvals"]
+
+    plt.plot(times_Rothe,dipmom_Rothe,label="Rothe_linear")
+except:
+    pass
+
 plt.legend()
 plt.savefig("dipole_moment.png")
 plt.show()
 plt.close()
 
+"""
 for i in range(0,Cvals_gauss.shape[0],20):
     plt.figure()
     plt.title('Time: %.3f'%times[i])
@@ -45,3 +52,4 @@ for i in range(0,Cvals_gauss.shape[0],20):
         os.mkdir("both")
         plt.savefig("both/orbitals_t=%.3f.png"%times[i])
     plt.close()
+"""